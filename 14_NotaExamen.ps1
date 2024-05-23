dc3f9e0dcbe885189d4b0ed6666846bfe5717c5d
paye3 <joanpayeras.91@gmail.com>
2024-05-23 19:07:42 +0200

    Canvis i programa 15

---
+++ 14_NotaExamen.ps1
@@ -0,0 +1,20 @@
+Write-Host "Introduce tu nota de examen"
+$nota = Read-Host
+
+if ($nota -ge 0 -and $nota -lt 5){
+    echo "INSUFICIENTE"
+} elseif ($nota --ge 5 -and $nota -lt 6) {
+    <# Action when this condition is true #>
+    echo "Suficiente pincheee!!!"
+} elseif ($nota -ge 6 -and $nota -lt 7) {
+    <# Action when this condition is true #>
+    echo "Solo bien, esfuerzate más!!"
+} elseif ($nota -ge 7 -and $nota -lt 9) {
+    <# Action when this condition is true #>
+    echo "notable..."
+} elseif ($nota -ge 9 -and $nota -le 10) {
+    <# Action when this condition is true #>
+    echo "¿A quien has copiado?"
+} else {
+    echo "La nota introducida.... Picha, no inventes!!"
+}
