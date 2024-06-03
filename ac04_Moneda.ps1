7c60846fd15df3ae2376557b05bd17b3e2605d03
paye3 <joanpayeras.91@gmail.com>
2024-06-03 17:36:18 +0200

    Update ac04_Moneda.ps1

---
+++ ac04_Moneda.ps1
@@ -0,0 +1,16 @@
+Write-Host "Introdueix cara o creu (c/c):"
+$opcio = Read-Host.ToLower()
+
+$resultat = Get-Random -Minimum 1 -Maximum 2
+
+if ($resultat -eq 1) {
+  $caraCreu = "cara"
+} else {
+  $caraCreu = "creu"
+}
+
+if ($opcio -eq $caraCreu) {
+  Write-Output "Has guanyat!"
+} else {
+  Write-Output "Has perdut!"
+}
